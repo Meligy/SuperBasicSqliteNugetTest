00c47ebac9a0be3d2236a7fbe5e6c4f07928fb87
Mohamed Meligy <eng.meligy@gmail.com>
2011-05-19 02:10:01 +1000

    Code similar to blog post just added the connectionstring

---
+++ PlaySqliteNuGet/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+
+namespace PlaySqliteNuGet
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var sessionFactory = ConfigureSessionFactory();
+            using(sessionFactory)
+            using(sessionFactory.OpenSession())
+            {
+                Console.WriteLine("Seems SQLite works nicely");
+            }
+        }
+
+        private static ISessionFactory ConfigureSessionFactory()
+        {
+            var config = new Configuration();
+
+            config.DataBaseIntegration(db =>
+            {
+                db.Dialect<SQLiteDialect>();
+                db.ConnectionStringName = "Sqlite_InMemory";
+            });
+
+            return config.BuildSessionFactory();
+        }
+    }
+}
